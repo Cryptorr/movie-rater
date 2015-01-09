16d157b7de4eda556a498795f779702637a83e06
Sergiovan <sergiovanpas@gmail.com>
2015-01-09 18:00:32 +0100

    Started search thing, errors

--- movie-rater/routes/index.js
+++ movie-rater/routes/index.js
@@ -1,5 +1,40 @@
 var express = require('express');
 var router = express.Router();
+var querystring = require('querystring');
+var https = require('https');
+
+var APIKEY = 'a283f3647d2484cd1b3dad05152d7766';
+
+function movieDB(data, cb){
+
+	var host = 'https://api.themoviedb.org';
+	var endpoint = '/3/genre/movie/list';
+	endpoint += '?api_key=' + APIKEY;
+	endpoint += '&' + querystring.stringify(data);
+
+
+	var options = {
+	host: host,
+	path: endpoint,
+	method: 'GET'
+	};
+
+	var req = https.request(options, function(res) {
+		res.setEncoding('utf-8');
+
+		var responseString = '';
+
+		res.on('data', function(data) {
+		  responseString += data;
+		});
+
+		res.on('end', function() {
+		  //console.log(responseString);
+		  var responseObject = JSON.parse(responseString);
+		  cb(responseObject);
+		});
+	});
+}
 
 //Get homepage
 router.get('/', function(req, res) {
@@ -9,6 +44,13 @@ router.get('/', function(req, res) {
 //Get browse page
 router.get('/browse', function(req, res) {
   res.render('browse', { title: 'Movie Rater App - Browse' });
+});
+
+router.route('/browse')
+  .post(function(req, res){
+  	movieDB(req.body, function(res){
+  		console.log(res);
+  	});
 });
 
 //Get report page
