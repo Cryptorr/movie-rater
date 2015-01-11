f6a2efc6546453eff51416f771618be7ac5ef132
Brentajacied <brentjuh@live.nl>
2015-01-11 14:23:23 +0100

    Adds removed stuff

--- movie-rater/routes/index.js
+++ movie-rater/routes/index.js
@@ -68,7 +68,10 @@ router.get('/login', function(req, res) {
 
 //Get movie page
 router.get('/movie/:id', function(req, res) {
-	res.render('movie', { title: 'Movie Rater App - ', id: id });
+	movieDB(req.body, '/3/movie/' + req.params.id, function(data){
+		console.log(data);
+		res.render('movie', { title: 'Movie Rater App - ' + data.title, data: data,  });
+	});
 });
 
 
